14f999651f26fdc16c8b7380934c8c9f6a022b1d
boriskisiov <boriskisiov@gmail.com>
2023-10-19 16:56:53 +0300

    Add project files.

---
+++ WhileLoop/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace WhileLoop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1;
+            while(i < 5)
+            {
+                Console.WriteLine(i);
+                i++;
+            }
+        }
+    }
+}
